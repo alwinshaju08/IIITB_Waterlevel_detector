c681d9c8a5a1a837961f698cf0c1f8eec4c21ce5
Alwin Shaju <69166205+alwinshaju08@users.noreply.github.com>
2023-10-07 14:24:15 +0530

    Update water_level.c

--- water_level.c
+++ water_level.c
@@ -16,34 +16,50 @@ void monitorWaterLevel() {
     int buzzerPin=0; // Replace with the actual GPIO pin connected to the buzzer
     int buzzer_reg;
     buzzer_reg = buzzerPin*2;
-     asm(
+     asm volatile(
 	"or x30, x30, %0\n\t" 
-	:"=r"(buzzer_reg));
+	:
+	:"r"(buzzer_reg)
+	:"x30"
+	);
     
-    asm(
+    asm volatile(
 	"andi %0, x30, 1\n\t"
-	:"=r"(liquidSensorPin));
+	:"=r"(liquidSensorPin)
+	:
+	:
+	);
     
 
     while (1) {
+       // int Liquid_level = digital_read(liquidSensorPin);
+        //printf("Powered By Techeonics ");
+        //printf("Liquid_level= %d\n", Liquid_level);
+
         if (liquidSensorPin) {
             // Simulate activating the buzzer (replace with actual buzzer control)
             //digital_write(buzzerPin, 1;
             //printf("Buzzer is ON\n");
             buzzerPin = 1;
             buzzer_reg = buzzerPin*2;
-            asm(
+            asm volatile(
 		"or x30, x30, %0\n\t" 
-		:"=r"(buzzer_reg));
+		:
+		:"r"(buzzer_reg)
+		:"x30"
+		);
         } else {
             // Simulate deactivating the buzzer (replace with actual buzzer control)
             //digital_write(buzzerPin, 0);
             //printf("Buzzer is OFF\n");
             buzzerPin = 0;
             buzzer_reg = buzzerPin*2;
-            asm(
+            asm volatile(
 		"or x30, x30, %0\n\t" 
-		:"=r"(buzzer_reg));
+		:
+		:"r"(buzzer_reg)
+		:"x30"
+		);
         }
     }
 }
