e0578f71df8a0a90f1c98ced67cf5802bca4ccd9
Alwin Shaju <69166205+alwinshaju08@users.noreply.github.com>
2023-10-24 20:05:51 +0530

    Update water_level.c

--- water_level.c
+++ water_level.c
@@ -1,24 +1,42 @@
+/*
+#include<stdio.h>
+#include<stdlib.h>
+*/
+
 int main() {
+    //int test,test1,test2,test3,test4;
     int liquidSensorPin;
     int buzzerPin = 0;
-    int solenoid = 1; // Additional output pin
     int buzzer_reg;
+    int solenoid ;
     int solenoid_reg; // Register for the solenoid
     int switchValue;  // Input switch pin
-
+    int mask1 = 0xFFFFFFF3;
     buzzer_reg = buzzerPin * 4;
     solenoid_reg = solenoid * 8; // Calculate the register for the solenoid
 
     asm volatile(
-        "and x30, x30, 0xFFFFFFF3\n\t"
+        "and x30, x30, %2\n\t"
         "or x30, x30, %0\n\t"
         "or x30, x30, %1\n\t"
         :
-        : "r"(buzzer_reg), "r"(solenoid_reg)
+        : "r"(buzzer_reg), "r"(solenoid_reg),"r"(mask1)
         : "x30"
     );
-
-    while (1) {
+     /*
+	    asm volatile(
+    	"addi x30, x30, 0\n\t"
+    	:"=r"(test)
+    	:
+    	:"x30"
+    	);
+    	printf("Test = %d\n",test);
+        printf("LiquidSensorPin = %d, switchvalue=%d,buzzer_reg = %d,  solenoid_reg=%d\n",liquidSensorPin,switchValue,buzzer_reg,solenoid_reg);
+	//printf("Inside while 1\n");*/
+    	//int z;
+//for(z=0;z<1;z++)
+    while (1) 
+    {
         
         asm volatile(
             "andi %0, x30, 1\n\t" // Assuming switch is connected to bit 3 (8 in binary)
@@ -26,6 +44,19 @@ int main() {
             :
             :
         );
+
+        int solenoid = 1;
+        
+	/*asm volatile(
+    	"addi %0, x30, 0\n\t"
+    	:"=r"(test1)
+    	:
+    	:"x30"
+    	);
+    	printf("test1 = %d\n",test1);*/
+        //switchValue=0;
+	//printf("Reset Button_val=%d\n",reset_button);
+	
 
         if (switchValue) {
             // Simulate activating the buzzer and solenoid (replace with actual control)
@@ -35,6 +66,14 @@ int main() {
                 :
                 :
             );
+           /*asm volatile(
+    	"addi %0, x30, 0\n\t"
+    	:"=r"(test2)
+    	:
+    	:"x30"
+    	);
+    	printf("test2 = %d\n",test2);*/
+        //liquidSensorPin=0;
 
             if (liquidSensorPin) {
                 // Simulate activating the buzzer and solenoid (replace with actual control)
@@ -43,6 +82,13 @@ int main() {
                 // printf("Buzzer ON and solenoid are Off\n");
                 buzzerPin = 1;
                 solenoid = 0;
+               /* asm volatile(
+    	"addi %0, x30, 0\n\t"
+    	:"=r"(test3)
+    	:
+    	:"x30"
+    	);
+    	printf("test3 = %d\n",test3);*/
             } else {
                 // Simulate deactivating the buzzer and solenoid (replace with actual control)
                 // digital_write(buzzerPin, 0);
@@ -50,6 +96,13 @@ int main() {
                 // printf("Buzzer OFF and solenoid are ON\n");
                 buzzerPin = 0;
                 solenoid = 1;
+                 /*asm volatile(
+    	"addi %0, x30, 0\n\t"
+    	:"=r"(test3)
+    	:
+    	:"x30"
+    	);
+    	printf("test3 = %d\n",test3);*/
             }
 
             // Update the corresponding registers for the buzzer and solenoid
@@ -57,13 +110,25 @@ int main() {
             solenoid_reg = solenoid * 8;
 
             asm volatile(
-                "and x30, x30, 0xFFFFFFF3\n\t"
+                "and x30, x30, %2\n\t"
                 "or x30, x30, %0\n\t"
                 "or x30, x30, %1\n\t"
                 :
-                : "r"(buzzer_reg), "r"(solenoid_reg)
+                : "r"(buzzer_reg), "r"(solenoid_reg),"r"(mask1)
                 : "x30"
             );
+    /*      
+    asm volatile(
+    	"addi %0, x30, 0\n\t"
+    	:"=r"(test4)
+    	:
+    	:"x30"
+    	);
+    	printf("Test4 = %d\n",test4);
+    printf("LiquidSensorPin = %d, switchvalue=%d,buzzer_reg = %d,  solenoid_reg=%d\n",liquidSensorPin,switchValue,buzzer_reg,solenoid_reg);
+    
+    */
+
         }
     }
 
