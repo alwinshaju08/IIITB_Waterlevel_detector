0e344d49f4fbdcfc12d897118798b165401b21f1
Alwin Shaju <69166205+alwinshaju08@users.noreply.github.com>
2023-10-24 17:13:29 +0530

    Update water_level.c

--- water_level.c
+++ water_level.c
@@ -4,12 +4,13 @@ int main() {
     int solenoid = 1; // Additional output pin
     int buzzer_reg;
     int solenoid_reg; // Register for the solenoid
-    int switchValue; // Input switch pin
+    int switchValue;  // Input switch pin
 
     buzzer_reg = buzzerPin * 4;
     solenoid_reg = solenoid * 8; // Calculate the register for the solenoid
 
     asm volatile(
+        "and x30, x30, 0xFFFFFFF3\n\t"
         "or x30, x30, %0\n\t"
         "or x30, x30, %1\n\t"
         :
@@ -38,15 +39,15 @@ int main() {
             if (liquidSensorPin) {
                 // Simulate activating the buzzer and solenoid (replace with actual control)
                 // digital_write(buzzerPin, 1);
-                // digital_write(solenoid, 1);
-                // printf("Buzzer and solenoid are ON\n");
+                // digital_write(solenoid, 0);
+                // printf("Buzzer ON and solenoid are Off\n");
                 buzzerPin = 1;
                 solenoid = 0;
             } else {
                 // Simulate deactivating the buzzer and solenoid (replace with actual control)
                 // digital_write(buzzerPin, 0);
-                // digital_write(solenoid, 0);
-                // printf("Buzzer and solenoid are OFF\n");
+                // digital_write(solenoid, 1);
+                // printf("Buzzer OFF and solenoid are ON\n");
                 buzzerPin = 0;
                 solenoid = 1;
             }
@@ -56,6 +57,7 @@ int main() {
             solenoid_reg = solenoid * 8;
 
             asm volatile(
+                "and x30, x30, 0xFFFFFFF3\n\t"
                 "or x30, x30, %0\n\t"
                 "or x30, x30, %1\n\t"
                 :
